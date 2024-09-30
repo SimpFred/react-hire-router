9d85755940dab4a0f529cee16e52d1dfdc2aceb6
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-09-30 15:14:46 +0200

    Refactor Dashboard component to include loading state and separate PeopleList for hired and non-hired people

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,23 +1,27 @@
-import { useState } from 'react'
-import PeopleList from './components/PeopleList'
+import PeopleList from "./components/PeopleList";
+import PropTypes from "prop-types";
 
-function Dashboard(props) {
-  const { hiredPeople } = props
-
-  const [people, setPeople] = useState([])
+function Dashboard({ hiredPeople, people }) {
+  if (people.length < 1) return <p>Loading...</p>;
 
   return (
     <main className="dashboard-layout">
       <section>
         <h2>People</h2>
-        <PeopleList people={people} />
+        <PeopleList isHiredList={false} people={people} />
       </section>
       <section>
         <h2>Hired People</h2>
-        <PeopleList people={hiredPeople} />
+        <PeopleList isHiredList={true} people={hiredPeople} />
       </section>
     </main>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
+
+Dashboard.propTypes = {
+  hiredPeople: PropTypes.array,
+  setHiredPeople: PropTypes.func,
+  people: PropTypes.array,
+};
