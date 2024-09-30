29ef034f7b5aa14b16c9a67a031e6970e6d1f614
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-09-30 15:05:06 +0200

    Refactor App component to include routing and fetch data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Link, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import PersonProfile from "./pages/PersonProfile";
 
 export default function App() {
-  const [hiredPeople, setHiredPeople] = useState([])
+  const [hiredPeople, setHiredPeople] = useState([]);
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=50")
+      .then((response) => response.json())
+      .then((data) => setPeople(data.results));
+  }, []);
 
   return (
     <>
@@ -10,10 +20,49 @@ export default function App() {
         <h1>Hire Your Team</h1>
         <nav>
           <ul>
-            <li>Dashboard</li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
           </ul>
         </nav>
       </header>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <main>
+              <h2>Welcome to the Hire Your Team app!</h2>
+            </main>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={<Dashboard hiredPeople={hiredPeople} people={people} />}
+        />
+        <Route
+          path="/person-profile/:id"
+          element={
+            <PersonProfile
+              isEditMode={false}
+              people={people}
+              setHiredPeople={setHiredPeople}
+              hiredPeople={hiredPeople}
+            />
+          }
+        />
+
+        <Route
+          path="/edit/:id"
+          element={
+            <PersonProfile
+              isEditMode={true}
+              people={hiredPeople}
+              setHiredPeople={setHiredPeople}
+              hiredPeople={hiredPeople}
+            />
+          }
+        />
+      </Routes>
     </>
-  )
+  );
 }
